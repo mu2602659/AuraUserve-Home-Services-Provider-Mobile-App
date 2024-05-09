f8dd83551d146a05567fbe13f1397c1391336d26
Arslan Saeed <saeedarslan253@gmail.com>
2024-05-09 13:44:39 +0500

    ch

--- Services/HomeCareScreen.js
+++ Services/HomeCareScreen.js
@@ -1,39 +1,76 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, Image, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { ArrowLeftIcon } from 'react-native-heroicons/solid';
 import { useNavigation } from '@react-navigation/native';
-
+import { SliderBox } from 'react-native-image-slider-box';
+import axios from 'axios';
+import { IMG_URL } from '../config/ip_address';
 const HomeCareScreen = () => {
+  const navigation = useNavigation(); // Initialize useNavigation hook
+  const [homeCareImages, setHomeCareImages] = useState([]);
+
+  useEffect(() => {
+    fetchHomeCareImages();
+  }, []);
+
+  const fetchHomeCareImages = async () => {
+    try {
+      const response = await axios.get(`${IMG_URL}/post-images?service=HomeCare Solutions`);
+      setHomeCareImages(response.data);
+    } catch (error) {
+      console.error('Error fetching home care images:', error);
+    }
+  };
+
+  const navigateToPostDetails = (post) => {
+    navigation.navigate('PostDetails', { post });
+  };
 
   const HomeCareData = [
     { id: '1', name: 'TailoringScreen', displayName: 'Tailoring', icon: require('../assets/icons/headlight.png') },
     { id: '2', name: 'CareTakerScreen', displayName: 'Care Taker', icon: require('../assets/icons/oil.png') },
-     { id: '3', name: 'MaidScreen', displayName: 'Maid', icon: require('../assets/icons/motorcy.png') },
+    { id: '3', name: 'MaidScreen', displayName: 'Maid', icon: require('../assets/icons/motorcy.png') },
     { id: '4', name: 'ChefScreen', displayName: 'Chef', icon: require('../assets/icons/wiper.png') },
-    
     // Add more services as needed
   ];
- const renderServiceBlock = (service) => (
-  <TouchableOpacity
-    key={service.id}
-    style={styles.serviceBlock}
-    onPress={() => {
-      console.log(`Navigating to ${service.name}`);
-      navigation.navigate(service.name);
-    }}
-  >
-    <View style={styles.serviceContent}>
-      <Image source={service.icon} style={styles.serviceIcon} />
-      <Text style={styles.serviceName}>{service.displayName}</Text>
-    </View>
-  </TouchableOpacity>
-);
+
+  const renderServiceBlock = (service) => (
+    <TouchableOpacity
+      key={service.id}
+      style={styles.serviceBlock}
+      onPress={() => {
+      console.log(`Navigating to Booking Page for ${service.displayName}`);
+      navigation.navigate('Services', { screen: 'Booking' }); // Navigate to BookingScreen
+      }}
+    >
+      <View style={styles.serviceContent}>
+        <Image source={service.icon} style={styles.serviceIcon} />
+        <Text style={styles.serviceName}>{service.displayName}</Text>
+      </View>
+    </TouchableOpacity>
+  );
 
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
-       
+      <View style={styles.sliderContainer}>
+          <SliderBox
+            images={homeCareImages.map(image => ({ uri: `data:image/jpeg;base64,${image.imageData}`, title: image.title }))}
+            sliderBoxHeight={200}
+            dotColor="#FFEE58"
+            inactiveDotColor="#90A4AE"
+            paginationBoxVerticalPadding={20}
+            autoplay
+            circleLoop
+            resizeMethod={'resize'}
+            resizeMode={'cover'}
+            paginationBoxStyle={styles.paginationBoxStyle}
+            dotStyle={styles.dotStyle}
+            ImageComponentStyle={styles.imageComponentStyle}
+            imageLoadingColor="#2196F3"
+            onCurrentImagePressed={(index) => navigateToPostDetails(homeCareImages[index])}
+          />
+        </View>
         <View style={styles.gridContainer}>
           {HomeCareData.map((service) => renderServiceBlock(service))}
         </View>
@@ -41,14 +78,11 @@ const HomeCareScreen = () => {
     </SafeAreaView>
   );
 };
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#FFFFFF',
-  },
-  header: {
-    alignItems: 'center',
-    padding: 16,
   },
   gridContainer: {
     justifyContent: 'space-around',
@@ -56,10 +90,10 @@ const styles = StyleSheet.create({
   },
   serviceBlock: {
     width: '100%',
-    aspectRatio: 12/3,
+    aspectRatio: 12 / 3,
     backgroundColor: '#F5F5F5',
     borderRadius: 10,
-    marginVertical: 8, // Adjust vertical margin as needed
+    marginVertical: 8,
     justifyContent: 'center',
     alignItems: 'center',
     borderWidth: 2,
@@ -70,18 +104,45 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
- serviceIcon: {
-  width: 50, // Example width value in pixels
-  height: 50, // Example height value in pixels
-  resizeMode: 'contain',
-  marginLeft: 30,
-},
-
+  serviceIcon: {
+    width: 50,
+    height: 50,
+    resizeMode: 'contain',
+    marginLeft: 30,
+  },
   serviceName: {
     fontSize: 18,
     fontWeight: 'bold',
     textAlign: 'center',
     flex: 1,
   },
+  sliderContainer: {
+    marginVertical: 10,
+    paddingHorizontal: 10,
+  },
+  paginationBoxStyle: {
+    position: 'absolute',
+    bottom: 0,
+    padding: 0,
+    alignItems: 'center',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    paddingVertical: 10
+  },
+  dotStyle: {
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+    marginHorizontal: 0,
+    padding: 0,
+    margin: 0,
+    backgroundColor: 'rgba(128, 128, 128, 0.92)'
+  },
+  imageComponentStyle: {
+    borderRadius: 15,
+    width: '97%',
+    marginTop: 5
+  },
 });
+
 export default HomeCareScreen;
--- Services/MaintenanceScreen.js
+++ Services/MaintenanceScreen.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, Image, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { ArrowLeftIcon } from 'react-native-heroicons/solid';
 import { useNavigation } from '@react-navigation/native';
+import { SliderBox } from 'react-native-image-slider-box';
+import axios from 'axios';
+import { IMG_URL } from '../config/ip_address';
 
 const MaintenanceScreen = () => {
+  const navigation = useNavigation(); // Initialize useNavigation hook
+  const [maintenanceImages, setMaintenanceImages] = useState([]);
 
+  useEffect(() => {
+    fetchMaintenanceImages();
+  }, []);
+
+  const fetchMaintenanceImages = async () => {
+    try {
+      const response = await axios.get(`${IMG_URL}/post-images?service=Maintenance`);
+      setMaintenanceImages(response.data);
+    } catch (error) {
+      console.error('Error fetching maintenance images:', error);
+    }
+  };
+
+  const navigateToPostDetails = (post) => {
+    navigation.navigate('PostDetails', { post });
+  };
   const MaintenanceData = [
     { id: '1', name: 'ElectricServicesScreen', displayName: 'Electric Services', icon: require('../assets/icons/electrical-service.png') },
     { id: '2', name: 'RoofingServicesScreen', displayName: 'Roofing services', icon: require('../assets/icons/roof.png') },
@@ -17,27 +37,45 @@ const MaintenanceScreen = () => {
     { id: '8', name: 'WDRScreen', displayName: 'Windows and Doors Replacement', icon: require('../assets/icons/window.png') },
     { id: '9', name: 'AluminiumGlassScreen', displayName: 'Aluminium, Glass', icon: require('../assets/icons/aluminium.png') },
     { id: '10', name: 'WeldingScreen', displayName: 'Welding', icon: require('../assets/icons/welding.png') },
-  
   ];
+
   const renderServiceBlock = (service) => (
-  <TouchableOpacity
-    key={service.id}
-    style={styles.serviceBlock}
-    onPress={() => {
-      console.log(`Navigating to ${service.name}`);
-      navigation.navigate(service.name);
-    }}
-  >
-    <View style={styles.serviceContent}>
-      <Image source={service.icon} style={styles.serviceIcon} />
-      <Text style={styles.serviceName}>{service.displayName}</Text>
-    </View>
-  </TouchableOpacity>
-);
+    <TouchableOpacity
+      key={service.id}
+      style={styles.serviceBlock}
+       onPress={() => {
+      console.log(`Navigating to Booking Page for ${service.displayName}`);
+      navigation.navigate('Services', { screen: 'Booking' }); // Navigate to BookingScreen
+      }}
+    >
+      <View style={styles.serviceContent}>
+        <Image source={service.icon} style={styles.serviceIcon} />
+        <Text style={styles.serviceName}>{service.displayName}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
-       
+      <View style={styles.sliderContainer}>
+          <SliderBox
+            images={maintenanceImages.map(image => ({ uri: `data:image/jpeg;base64,${image.imageData}`, title: image.title }))}
+            sliderBoxHeight={200}
+            dotColor="#FFEE58"
+            inactiveDotColor="#90A4AE"
+            paginationBoxVerticalPadding={20}
+            autoplay
+            circleLoop
+            resizeMethod={'resize'}
+            resizeMode={'cover'}
+            paginationBoxStyle={styles.paginationBoxStyle}
+            dotStyle={styles.dotStyle}
+            ImageComponentStyle={styles.imageComponentStyle}
+            imageLoadingColor="#2196F3"
+            onCurrentImagePressed={(index) => navigateToPostDetails(maintenanceImages[index])}
+          />
+        </View>
         <View style={styles.gridContainer}>
           {MaintenanceData.map((service) => renderServiceBlock(service))}
         </View>
@@ -45,14 +83,11 @@ const MaintenanceScreen = () => {
     </SafeAreaView>
   );
 };
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#FFFFFF',
-  },
-  header: {
-    alignItems: 'center',
-    padding: 16,
   },
   gridContainer: {
     justifyContent: 'space-around',
@@ -60,10 +95,10 @@ const styles = StyleSheet.create({
   },
   serviceBlock: {
     width: '100%',
-    aspectRatio: 12/3,
+    aspectRatio: 12 / 3,
     backgroundColor: '#F5F5F5',
     borderRadius: 10,
-    marginVertical: 8, // Adjust vertical margin as needed
+    marginVertical: 8,
     justifyContent: 'center',
     alignItems: 'center',
     borderWidth: 2,
@@ -74,19 +109,46 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
- serviceIcon: {
-  width: 50, // Example width value in pixels
-  height: 50, // Example height value in pixels
-  resizeMode: 'contain',
-  marginLeft: 30,
-},
-
+  serviceIcon: {
+    width: 50,
+    height: 50,
+    resizeMode: 'contain',
+    marginLeft: 30,
+  },
   serviceName: {
     fontSize: 18,
     fontWeight: 'bold',
     textAlign: 'center',
     flex: 1,
   },
+  sliderContainer: {
+    marginVertical: 10,
+    paddingHorizontal: 10,
+  },
+  paginationBoxStyle: {
+    position: 'absolute',
+    bottom: 0,
+    padding: 0,
+    alignItems: 'center',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    paddingVertical: 10
+  },
+  dotStyle: {
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+    marginHorizontal: 0,
+    padding: 0,
+    margin: 0,
+    backgroundColor: 'rgba(128, 128, 128, 0.92)'
+  },
+  imageComponentStyle: {
+    borderRadius: 15,
+    width: '97%',
+    marginTop: 5
+  },
+  
 });
 
 export default MaintenanceScreen;
